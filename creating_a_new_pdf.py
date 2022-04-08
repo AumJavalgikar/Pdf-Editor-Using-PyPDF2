41edf4928b6e0cc75549a61551d3b38a7a133b02
aum <aumjavalgikar@gmail.com>
2022-04-08 11:09:24 +0530

    learned how to merge pdfs :D

---
+++ creating_a_new_pdf.py
@@ -0,0 +1,22 @@
+import PyPDF2
+
+pdfFile1 = open('pdfs/meetingminutes.pdf', 'rb')
+pdfFile2 = open('pdfs/meetingminutes2.pdf', 'rb')
+
+pdfReader1 = PyPDF2.PdfFileReader(pdfFile1)
+pdfReader2 = PyPDF2.PdfFileReader(pdfFile2)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+for page in range(pdfReader1.numPages):
+    pageObj = pdfReader1.getPage(page)
+    pdfWriter.addPage(pageObj)
+
+for page in range(pdfReader2.numPages):
+    pageObj = pdfReader2.getPage(page)
+    pdfWriter.addPage(pageObj)
+
+pdfOutputFile = open('pdfs/combined_minutes', 'wb')
+pdfWriter.write(pdfOutputFile)
+pdfOutputFile.close()
+pdfFile1.close()
+pdfFile2.close()
